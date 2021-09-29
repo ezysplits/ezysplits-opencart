e9cbee3b011dd1b873b0db378c9a28fda09b8fcd
Prem <prem@ezycredits.com>
2021-09-30 00:32:10 +0530

    Added ezysplit extension for opencart 3

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ezysplits-opencart
+## Ezyspilits Extension for Opencart
+
+This extension utilizes Ezyspilits API and provides seamless integration with OpenCart, allowing payments for Indian merchants.
+
+### Installation
+
+Copy all files/folders recursively to opencart installation directory.
+
+Install vqmod by running "www.yourstore.com/vqmod/install" in your browser('www.yourstore.com' should be your store domain name).
+
+Go to Admin Panel, Extensions->Payments and install the Ezyspilits extension.
+
+Click on Edit next to Razorpay and do the following:
+
+- Add your Merchant Id
+- Add your Secret Key
+- Change plugin status to Enabled
+- Select Payment Mode Sandbox/Live
+
+Save the plugin settings
+
+### Development
+
+- The `master` branch holds the plugin for OpenCart 3
---
+++ catalog/language/en-gb/extension/payment/ezysplits.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']                    = '<a class="ezysplits_icon" href="https://www.ezysplits.com" target="_blank"><img src="catalog/view/theme/default/image/favicon.png" alt="Ezysplits" title="Ezysplits" style="border: 1px solid #EEEEEE;margin-top: 5px;width: 25px;" /></a> Pay by Ezysplits at 0% EMI';
+$_['text_description']              = 'Items on %s Order No: %s';
+$_['ezysplits_api_error']           = 'API call error : ';
+$_['text_redirect']                 = 'Redirecting...';
+$_['ezysplits_payment_failed']      = 'Your transaction has failed. Please try again.';
+$_['ezysplits_payment_pending']     = 'Your transaction is yet to be confirmed. Please wait for some time.';
+$_['ezysplits_payment_cancelled']   = 'Your transaction has been cancelled. Please try again.';
+$_['ezysplits_signature_mismatch']  = 'Failed to verify the response.';
